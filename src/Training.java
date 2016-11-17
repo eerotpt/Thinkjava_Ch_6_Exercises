4538f07384f3d89504832ab919c5cd26fd70cd83
eerotpt <eero.sakson@tptlive.ee>
2016-11-17 11:45:59 +0200

    11.45

--- src/Training.java
+++ src/Training.java
@@ -3,15 +3,22 @@
  */
 public class Training {
 
-    public static double calculateArea(double radius) {
-        double result = Math.PI * radius * radius;
-        return result;
+    public static void countup(int n) {
+        if (n == 0) {
+            System.out.println("Blastoff!");
+        } else {
+            countup(n - 1);
+            System.out.println("n="+n);
+            int s=n*n;
+            System.out.println(s);
+
+        }
     }
+
     public static void main(String[] args){
-
-        calculateArea(9);
-
-        System.out.println();
+        int x=2;
+        countup(3);
+        System.out.println("Have a nice day.");
     }
 }
 
